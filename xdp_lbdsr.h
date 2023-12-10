7a7f5ccbe3a76ddaa97263bfcb917b93ed0c4551
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-12-10 15:09:05 +0000

    Create xdp_lbdsr.h

---
+++ xdp_lbdsr.h
@@ -0,0 +1,18 @@
+struct serveraddr {
+  uint32_t ipaddr;
+  uint8_t macaddr[6];
+}
+
+struct dispatchmsg_t {
+   uint64_t timestamp;
+   uint32_t saddr;
+   uint32_t backendkey
+};
+
+struct five_tuple {
+    uint8_t  protocol;
+    uint32_t ip_source;
+    uint32_t ip_destination;
+    uint16_t port_source;
+    uint16_t port_destination;
+};

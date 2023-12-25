28ca5231a1662d972b0c219bc282a06f90719338
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-12-25 00:50:27 +0000

    Update xdp_lbdsr.h

--- xdp_lbdsr.h
+++ xdp_lbdsr.h
@@ -1,13 +1,10 @@
-struct serveraddr {
-  uint32_t ipaddr;
-  uint8_t macaddr[6];
-};
+#define IP_ADDRESS(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
+#define VIP_ADDRESS(x) (unsigned int)(192 + (168 << 8) + (25 << 16) + (x << 24))
 
-struct dispatchmsg_t {
-   uint64_t timestamp;
-   uint32_t saddr;
-   uint32_t backendkey;
-};
+#define LB 2
+#define BACKEND_A 3
+#define BACKEND_B 4
+#define VIP 10
 
 struct five_tuple {
     uint8_t  protocol;
